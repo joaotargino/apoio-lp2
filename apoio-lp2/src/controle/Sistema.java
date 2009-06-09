5b9a3958150926efdde237980d2750d985b00cb7
jessicadesousacg <jessicadesousacg@e8ce2fee-488e-11de-a3b0-c1e83b3b5a46>
2009-06-09 22:51:12 +0000

    testando =B

--- apoio-lp2/src/controle/Sistema.java
+++ apoio-lp2/src/controle/Sistema.java
@@ -4,6 +4,7 @@ package controle;
 //testado por jessica
 //testado por jessica win
 //jao diz: agora vaaaai!
+//jes diz: vai mermo? :B
 public interface Sistema {
 	
 	public boolean login() throws Exception;
