dd76404df75e9e94212284233c857a941296b739
joaotargino <joaotargino@e8ce2fee-488e-11de-a3b0-c1e83b3b5a46>
2009-06-26 06:25:30 +0000

    editar dados funcionando, enums

---
+++ trunk/apoio-lp2/src/controle/DadosUsuarioEnum.java
@@ -0,0 +1,8 @@
+package controle;
+
+public enum DadosUsuarioEnum {
+
+	LOGIN, SENHA, MATRICULA, NOME, EMAIL, TURMA;
+
+
+}
--- trunk/apoio-lp2/src/users/Aluno.java
+++ trunk/apoio-lp2/src/users/Aluno.java
@@ -3,19 +3,18 @@ package users;
 import controle.*;
 import disciplina.*;
 
-
-
 public class Aluno extends Usuario {
 
 	static final String NOVA_LINHA = System.getProperty("line.separator");
-	
-	public Aluno(String nome, String login, String senha, String matricula, String email, String turma) throws Exception {
-		super(nome, login, senha, matricula, email);
+
+	public Aluno(String nome, String login, String senha, String matricula,
+			String email, String turma) throws Exception {
+		super(nome, login, senha, matricula, email, turma);
 	}
-	
+
 	public boolean submeterExercicio(Exercicio exercicio) {
 		return Sistema.subemeterExercicio(getLogin(), exercicio);
-			
+
 	}
-	
+
 }
