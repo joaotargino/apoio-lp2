e76bcaab841b8fcde50f9aca2369f9b934faa37d
jessicadesousacg <jessicadesousacg@e8ce2fee-488e-11de-a3b0-c1e83b3b5a46>
2009-07-06 22:36:03 +0000

    teste1

--- apoio-lp2/src/interfaceUsuario/UserInterface.java
+++ apoio-lp2/src/interfaceUsuario/UserInterface.java
@@ -21,7 +21,7 @@ import disciplina.Exercicio;
  * @version 0.5  -  Junho / 2009
  */
 public class UserInterface {
-//teste
+//teste 1
 	static Scanner sc = new Scanner(System.in);
 
 	static String logado;
